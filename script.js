a81cff7a0b65050472cd10b94276fd0dc71609be
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-07-23 02:34:57 +0100

    firebase added

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-database.js";
+  import {
+    getAuth,
+    createUserWithEmailAndPassword,
+  } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCh0YDE8uMfIbUSi2qK6gkdBrvqMq3iO7M",
+    authDomain: "form-authentication-f0a32.firebaseapp.com",
+    databaseURL: "https://form-authentication-f0a32-default-rtdb.firebaseio.com",
+    projectId: "form-authentication-f0a32",
+    storageBucket: "form-authentication-f0a32.appspot.com",
+    messagingSenderId: "631471451920",
+    appId: "1:631471451920:web:f1993d6bc9d58a28243f95"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const auth = getAuth();
+  
+document.getElementById("submit").addEventListener('click', (e) => {
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    var username = document.getElementById('username').value
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        set(ref(database, 'users/' + user.uid), {
+          username: username,
+          email:email
+        })
+        alert( user + 'user created')
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(errorMessage)
+        // ..
+      });
+})
+
+
+
+
+
+
 //Global variables
 const form = document.getElementById("form");
 const firstName = document.getElementById("firstname");
@@ -11,10 +68,28 @@ const confirmPassword = document.getElementById("confirmPassword");
 //Add event listener
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
   checkInput();
+
+
+
+  if (isFormValid() == true) {
+    form.submit();
+  } else {
+    e.preventDefault();
+  }
 });
+
+function isFormValid() {
+  const inputContainers = form.querySelectorAll(".form-control");
+  let result = true;
+  inputContainers.forEach((container) => {
+    if (container.classList.contains("error")) {
+      result = false;
+    }
+  });
+
+  return result;
+}
 
 //Function to check input
 function checkInput() {
@@ -26,16 +101,25 @@ function checkInput() {
   const passwordValue = password.value.trim();
   const confirmPasswordValue = confirmPassword.value.trim();
 
+
+  let firstnameregex = /^[a-zA-Z\s]+$/;
+  let lastnameregex = /^[a-zA-Z\s]+$/;
   //Validate First Name
   if (firstNameValue === "") {
     setErrorFor(firstName, "First name cannot be blank");
-  } else {
+  } else if (!firstnameregex.test(firstNameValue)) {
+    setErrorFor(firstName, "Invalid First Name");
+  }
+  else {
     setSuccessFor(firstName);
   }
   //Validate Last Name
   if (lastNameValue === "") {
     setErrorFor(lastName, "First name cannot be blank");
-  } else {
+  }  else if (!lastnameregex.test(lastNameValue)) {
+    setErrorFor(lastName, "Invalid Last Name");
+  }
+  else {
     setSuccessFor(lastName);
   }
 
@@ -76,6 +160,7 @@ function checkInput() {
     setSuccessFor(confirmPassword);
   }
 }
+
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
   const small = formControl.querySelector("small");
